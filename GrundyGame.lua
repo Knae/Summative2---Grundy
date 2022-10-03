6e940f6fe5bccfbf14f8808d095d7373bc6ba9eb
Chuang Sing Kee <5388918+Knae@users.noreply.github.com>
2022-10-03 03:35:56 +0000

    -Completed base game loop -Initial implementation of easy Ai --error in GrundyGame, table being passed instead of an integer value

--- GrundyGame.lua
+++ GrundyGame.lua
@@ -4,19 +4,19 @@ function GrundyGame:new(startStack)
    GrundyGame[1] = startStack 
 end
 
-function GrundyGame.NewGame( startStack)
+function GrundyGame.NewGame(startStack)
   while GrundyGame[2] ~= nil do
     table.remove(GrundyGame)
   end
-
   GrundyGame[1] = startStack
 end
 
 function GrundyGame.CheckIfGameEnd()
-  local gameEnd = false;
+  local gameEnd = true;
   for key,value in ipairs(GrundyGame) do
-    if value <= 2 then
-      gameEnd = true
+    --print(value[1],value[2],value[3])
+    if value > 2 then
+      gameEnd = false
     end
   end
   return gameEnd
@@ -25,18 +25,67 @@ end
 function GrundyGame.StartSplitting( index )
   local currentDivisions = require("GrundyDivisions")
   local valueToDivide = GrundyGame[index]
-  
+
   if valueToDivide == nil then
     print("No value at that index")
     return false
   end
 
-  currentDivisions.FindPossibleDivisions(valueToDivide)
-  currentDivisions.PrintDivisions()
+  print("Splitting", valueToDivide )
+  currentDivisions:FindPossibleDivisions(valueToDivide)
+  currentDivisions:PrintDivisions()
 
-  print("Input the number corresponding to howw you want to split ",valueToDivide)
+  print("Input the number corresponding to how you want to split",valueToDivide)
   local chosenIndex = io.read("*n")
 
   print("Chosen Index was ", chosenIndex)
   
+  if currentDivisions[chosenIndex] == nil then
+    print("No data at that index")
+    return false
+  end
+  
+  GrundyGame[index] = currentDivisions[chosenIndex][2]
+  table.insert(GrundyGame,index,currentDivisions[chosenIndex][1])
+
+  currentDivisions.ClearTable()
+  return true
 end
+
+function GrundyGame.PrintStacks()
+  local stringToPrint = ""
+  local index = 0
+  print("Current Stacks:")
+  for key,value in ipairs(GrundyGame) do
+    index = index + 1
+    stringToPrint = stringToPrint .. "[" .. value .. "]"
+  end
+  print(stringToPrint)
+  return index
+end
+
+function GrundyGame.AI_Easy()
+  print("AI(Easy) is thinking.....")
+  local maxIndex = GrundyGame.PrintStacks()
+  local currentDivisions = require("GrundyDivisions")
+  local chosenStack = math.random(maxIndex)
+  local valueToDivide = GrundyGame[chosenStack]
+  
+  print("AI chose stack no:", chosenStack)
+  print("==============================")
+  currentDivisions:FindPossibleDivisions(valueToDivide)
+  print("AI had these possible options:")
+  print("==============================")
+  local maxDivisons = currentDivisions:PrintDivisions()
+
+  print("========= ")
+  local chosenIndex = math.random(maxDivisons)
+  print("AI chose:",chosenIndex)
+  
+  GrundyGame[chosenStack] = currentDivisions[chosenIndex][2]
+  table.insert(GrundyGame,chosenStack,currentDivisions[chosenIndex][1])
+
+  currentDivisions.ClearTable()
+  return true
+end
+return GrundyGame
--- main.lua
+++ main.lua
@@ -1,3 +1,35 @@
-game = require("GrundyGame")
-game.NewGame(7)
-game.StartSplitting(1)
+math.randomseed(os.time())
+local game = require("GrundyGame")
+local player1Turn = true;
+local aiPlayer = true
+
+print("Input the initial stack")
+local stackValue = io.read("*n")
+
+game.NewGame(stackValue)
+while(not game.CheckIfGameEnd()) do
+  print("========")
+  if player1Turn then
+    print("[Turn 1]")
+  else
+    print("[Turn 2]")
+  end
+  print("========")
+
+  if(aiPlayer and not player1Turn) then
+    game.AI_Easy()
+  else
+    game.PrintStacks()
+
+    print("Input the stack split:")
+    stackValue = io.read("*n")
+
+    if game.StartSplitting(stackValue) then
+     player1Turn = not player1Turn
+    end
+  end
+end
+
+print("Game has ended")
+print("Final state of stacts:")
+game.PrintStacks()
